5402acbe9224c7773f9173df01a756e3d9cfcdd4
blike <blike@blikeLinux1.com>
2017-07-27 15:34:32 +0300

    POST

--- server/server.js
+++ server/server.js
@@ -1,28 +1,40 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+var mongoose = require('./db/mongoose.js').mongoose;
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
+var User = require('./models/user.js');
 
-var newTodo = new Todo({
-  text: 'Cook dinner',
-  completed: true,
-  completedAt: 123
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) => {
+   var todo = new Todo({
+     text: req.body.text
+   });
+   todo.save().then((doc) => {
+     res.send(doc);
+   }, (e) => {
+     res.status(400).send(e);
+   });
 });
 
-newTodo.save().then((doc) => {
-  console.log('Saved Todo',JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save todo', e);
-})
+app.listen(3000, () => {
+  console.log('Started on port 3000')
+});
