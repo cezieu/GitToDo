516c5a1419d398395c14734049a5e2c0f9f34f16
blike <blike@blikeLinux1.com>
2017-07-31 14:37:15 +0300

    Added Get/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+const ObjectID = require('mongodb').ObjectID;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
@@ -52,6 +53,21 @@ app.get('/todos',(req,res) => {
   })
 });
 
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+if(!ObjectID.isValid(id)) {
+  return res.status(404).send();
+}
+    Todo.findById(id).then((todo) => {
+     if (!todo) {
+       return res.status(404).send();
+     }
+    res.status(200).send({todo});
+
+   }).catch((e) => {
+     res.status(400).send();
+   })
+});
 app.listen(3000, () => {
   console.log('Started on port 3000')
 });
